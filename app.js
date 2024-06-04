2ab108c2abfd9e7f9f3fbf648e5de7ee82393a98
Mathieu <math.legargasson@gmail.com>
2024-06-04 19:53:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config(); // est ce que c'est bon ? le .env à déja été mis dans gitignore
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var router = express.Router();
+const fetch = require("node-fetch");
+require('dotenv').config();
+
+
+const API_KEY_TMDB = process.env.API_KEY_TMDB;
+
+router.get('/movies', (req, res) => {
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_TMDB}`) // pourqoi il manquait le discover ?
+.then(response => response.json())
+.then(data => {
+    res.json({ movies: data.results }) // il manquait un s à result comment on trouve cette variable ?
+});
+});
+
+
+
+
+
+
+
+
+module.exports = router;
