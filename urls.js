43941326788c75cdcdaf205e09c7ea429c58099c
Mathieu <math.legargasson@gmail.com>
2024-06-04 20:46:40 +0200

    vercel en cours

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://vercel.com/mathieus-projects-4bb33ec6/my-moviz-backend';
+const BACKEND_URL = '';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://vercel.com/mathieus-projects-4bb33ec6/my-moviz-frontend';
+const FRONTEND_URL = '';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
