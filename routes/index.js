0dff557d6872f0da9ab5ae07991e66e1b346cbb2
Mathieu <math.legargasson@gmail.com>
2024-06-04 20:48:40 +0200

    vercel en cous

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const API_KEY_TMDB = process.env.API_KEY_TMDB;
 router.get('/movies', (req, res) => {
 fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY_TMDB}`) // pourqoi il manquait le discover ?
 .then(response => response.json())
-.then(data => {
+.then(data => {  
     res.json({ movies: data.results }) // il manquait un s à result comment on trouve cette variable ?
 });
 });
